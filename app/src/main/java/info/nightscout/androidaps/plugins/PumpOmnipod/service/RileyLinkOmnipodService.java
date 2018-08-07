553e7810078d8d96309908503f0c59d796599fc0
Artiom Kenibasov <artiom@gmail.com>
2018-08-07 16:02:51 +0200

    Added encoding to Omnipod service

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/service/RileyLinkOmnipodService.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/service/RileyLinkOmnipodService.java
@@ -25,6 +25,7 @@ import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.RileyLinkConst
 import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.RileyLinkUtil;
 import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.ble.RFSpy;
 import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.ble.RileyLinkBLE;
+import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.ble.defs.RileyLinkEncodingType;
 import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.ble.defs.RileyLinkTargetFrequency;
 import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.defs.RileyLinkTargetDevice;
 import info.nightscout.androidaps.plugins.PumpCommon.hw.rileylink.service.RileyLinkService;
@@ -53,6 +54,10 @@ public class RileyLinkOmnipodService extends RileyLinkService {
     OmnipodCommunicationManager omnipodCommunicationManager;
     private IBinder mBinder = new LocalBinder();
 
+    @Override
+    public RileyLinkEncodingType getEncoding() {
+        return RileyLinkEncodingType.FourByteSixByte;
+    }
 
     public RileyLinkOmnipodService() {
         super(MainApp.instance().getApplicationContext());
