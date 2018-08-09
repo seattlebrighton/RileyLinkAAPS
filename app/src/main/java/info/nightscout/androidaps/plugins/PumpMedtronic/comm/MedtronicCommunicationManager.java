c9122b1fa248c5e41ab57aed1d13556f6b008f17
Andy Rozman <aleksander.rozman@bearingpoint.com>
2018-08-09 10:22:00 +0100

    - getProfile set back to STD

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpMedtronic/comm/MedtronicCommunicationManager.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpMedtronic/comm/MedtronicCommunicationManager.java
@@ -579,7 +579,7 @@ public class MedtronicCommunicationManager extends RileyLinkCommunicationManager
 
         MedtronicUtil.setPumpDeviceState(PumpDeviceState.Active);
 
-        MedtronicCommandType commandType = MedtronicCommandType.GetBasalProfileB;
+        MedtronicCommandType commandType = MedtronicCommandType.GetBasalProfileSTD;
 
         for(int retries = 0; retries <= MAX_COMMAND_RETRIES; retries++) {
 
