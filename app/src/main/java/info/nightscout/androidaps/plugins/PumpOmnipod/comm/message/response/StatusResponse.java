b326f36af15ef36d07767d0c1b3b938d5f9555a7
Bryan <bryanbassett@gmail.com>
2018-10-10 01:14:54 -0400

    StatusResponse unit tests
    handle signed behavior

--- app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/comm/message/response/StatusResponse.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/PumpOmnipod/comm/message/response/StatusResponse.java
@@ -1,6 +1,5 @@
 package info.nightscout.androidaps.plugins.PumpOmnipod.comm.message.response;
 
-import org.apache.commons.lang3.NotImplementedException;
 import org.joda.time.Duration;
 
 import info.nightscout.androidaps.Constants;
@@ -10,7 +9,6 @@ import info.nightscout.androidaps.plugins.PumpOmnipod.comm.message.MessageBlockT
 import info.nightscout.androidaps.plugins.PumpOmnipod.defs.DeliveryStatus;
 import info.nightscout.androidaps.plugins.PumpOmnipod.defs.PodAlarm;
 import info.nightscout.androidaps.plugins.PumpOmnipod.defs.ReservoirStatus;
-import info.nightscout.utils.Round;
 
 public class StatusResponse extends MessageBlock {
     public final DeliveryStatus deliveryStatus;
@@ -20,30 +18,29 @@ public class StatusResponse extends MessageBlock {
     public final double insulinNotDelivered;
     public final byte podMessageCounter;
     public final PodAlarm alarms;
-    private final double reservoirLevel;
+    public final double reservoirLevel;
 
     public StatusResponse(byte[] encodedData) {
         super(encodedData);
 
-        this.deliveryStatus = DeliveryStatus.fromByte((byte) ((encodedData[1] & 0xF0) >> 4));
-        this.reservoirStatus = ReservoirStatus.fromByte((byte) (encodedData[1] & (byte)0x0F));
-        int minutes = ((encodedData[7] & 0x7F) << 6) + ((encodedData[8] & 0xFC) >> 2);
+        this.deliveryStatus = DeliveryStatus.fromByte((byte) ((encodedData[1] & 0xF0) >>> 4));
+        this.reservoirStatus = ReservoirStatus.fromByte((byte) (encodedData[1] & 0x0F));
+        int minutes = ((encodedData[7] & 0x7F) << 6) | ((encodedData[8] & 0xFC) >>> 2);
         this.activeTime = Duration.standardMinutes(minutes);
 
-        // 00022223 33333334
         int highInsulinBits = (encodedData[2] & 0x0F) << 9;
-        int middleInsulinBits = ((int)encodedData[3] & 0xFF) << 1;
-        int lowInsulinBits = (encodedData[4] & 0x80) >> 7;
+        int middleInsulinBits = (encodedData[3] & 0xFF) << 1;
+        int lowInsulinBits = (encodedData[4] & 0x80) >>> 7;
         this.insulin = Constants.PodPulseSize * (highInsulinBits | middleInsulinBits | lowInsulinBits);
-        this.podMessageCounter = (byte) (((encodedData[4] & 0xFF) >> 3) & 0x0F);
+        this.podMessageCounter = (byte) ((encodedData[4] & 0x78) >>> 3);
 
-        this.insulinNotDelivered = Constants.PodPulseSize * ((encodedData[4] & 0x03) << 8) + ((int)encodedData[5] & 0xFF);
-        this.alarms = new PodAlarm((byte) (((encodedData[6] & 0x7f) << 1) | ((encodedData[7] & 0xFF) >> 7)));
+        this.insulinNotDelivered = Constants.PodPulseSize * (((encodedData[4] & 0x03) << 8) | (encodedData[5] & 0xFF));
+        this.alarms = new PodAlarm((byte) (((encodedData[6] & 0x7f) << 1) | ((encodedData[7] & 0x80) >>> 7)));
 
         int resHighBits = ((encodedData[8] & 0x03) << 6);
-        int resLowBits = ((encodedData[9] & 0xFC) >> 2);
+        int resLowBits = ((encodedData[9] & 0xFC) >>> 2);
 
-        this.reservoirLevel = Math.round((double)((resHighBits + resLowBits)) * 50 / 255);
+        this.reservoirLevel = Math.round((double)((resHighBits | resLowBits)) * 50 / 255);
 
         this.encodedData = ByteUtil.substring(encodedData, 1, 9);
 
