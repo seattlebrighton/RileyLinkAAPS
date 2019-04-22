6594c464de46f24821f520c538ac4c41540897a9
Mark Brighton <seattlebrighton@gmail.com>
2019-04-22 14:34:11 -0700

    Update CancelDeliveryCommandUnitTests.java

--- app/src/test/java/info/nightscout/androidaps/plugins/pump/omnipod/comm/message/command/CancelDeliveryCommandUnitTests.java
+++ app/src/test/java/info/nightscout/androidaps/plugins/pump/omnipod/comm/message/command/CancelDeliveryCommandUnitTests.java
@@ -12,7 +12,7 @@ public class CancelDeliveryCommandUnitTests {
     @Test
     public void Encode_ValidParams_CorrectBytes()
     {
-        CancelDeliveryCommand command = new CancelDeliveryCommand(0x10203040, BeepType.Type3, true, false, true);
+        CancelDeliveryCommand command = new CancelDeliveryCommand(0x10203040, BeepType.TwoBips, true, false, true);
 
         byte[] expected = ByteUtil.fromHexString("1F051020304035");
         assertArrayEquals(expected, command.getRawData());
